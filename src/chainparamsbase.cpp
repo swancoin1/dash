2227f2e88d3d30bcd29d6c890c4367f69a33f968
swancoin1 <36109473+swancoin1@users.noreply.github.com>
2018-02-11 21:04:32 +0300

    Release 0.1.0.1
    Release 0.1.0.1

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9998;
+        nRPCPort = 9886; // 9998 -> 9886
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19998;
+        nRPCPort = 19886; // 19998 -> 19886
         strDataDir = "testnet3";
     }
 };
